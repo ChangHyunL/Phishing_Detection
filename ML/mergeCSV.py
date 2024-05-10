0ea6f8ce654146bdda62fe0c99bac2fb6f469960
eeche <dlckdgus200011@gmail.com>
2024-05-10 17:43:41 +0900

    Feat: 2차 데이터 전처리 완료

--- ML/mergeCSV.py
+++ ML/mergeCSV.py
@@ -1,25 +1,23 @@
 import csv
 import pandas as pd
 
-# file = 'C:/Users/dlckd/Desktop/2024-1학기/캡스톤디자인/Phising_Detection/ML/Datasets/processed_normal_url1.csv'
-# file1 = 'C:/Users/dlckd/Desktop/2024-1학기/캡스톤디자인/Phising_Detection/ML/Datasets/processed_normal_url.csv'
-# # CSV 파일 읽기
-# df = pd.read_csv(file)
+file = 'C:/Users/dlckd/Desktop/2024-1학기/캡스톤디자인/Phising_Detection/ML/Datasets/2nd/2nd_processed_malicious_url.csv'
+file1 = 'C:/Users/dlckd/Desktop/2024-1학기/캡스톤디자인/Phising_Detection/ML/Datasets/2nd/2nd_processed_malicious_url1.csv'
+# CSV 파일 읽기
+df = pd.read_csv(file)
 
-# # 중복된 행 제거
-# df = df.drop_duplicates(subset=df.columns[0])
+# 중복된 행 제거
+df = df.drop_duplicates(subset=df.columns[0])
 
-# # 결과를 새로운 CSV 파일로 저장
-# df.to_csv(file1, index=False)
-
-# merged_dataframe.to_csv('merged1.csv', index=False)
+# 결과를 새로운 CSV 파일로 저장
+df.to_csv(file1, index=False)
 
 
 # 입력 파일과 출력 파일 경로
-input_file = 'C:/Users/dlckd/Desktop/2024-1학기/캡스톤디자인/Phising_Detection/ML/Datasets/processed_normal_url.csv'
-output_file = 'C:/Users/dlckd/Desktop/2024-1학기/캡스톤디자인/Phising_Detection/ML/Datasets/processed_normal_url1.csv'
+# input_file = 'C:/Users/dlckd/Desktop/2024-1학기/캡스톤디자인/Phising_Detection/ML/Datasets/processed_malicious_url.csv'
+# output_file = 'C:/Users/dlckd/Desktop/2024-1학기/캡스톤디자인/Phising_Detection/ML/Datasets/processed_malicious_url1.csv'
 
-df = pd.read_csv(input_file)
+# df = pd.read_csv(input_file)
 
-df['isphishing'] = 0
-df.to_csv(output_file, index=False)
+# df['isphishing'] = 1
+# df.to_csv(output_file, index=False)
