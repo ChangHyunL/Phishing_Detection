ca2704d5d384400e60ad03b467fa1bf6e98abb4d
eeche <dlckdgus200011@gmail.com>
2024-05-13 02:48:21 +0900

    Feat: 테스트 데이터셋 생성

---
+++ ML/test.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+# 파일 경로
+# file_path = 'C:/Users/dlckd/Desktop/2024-1학기/캡스톤디자인/Phising_Detection/ML/Datasets/2nd/2nd_processed_malicious_url.csv'
+file_path = 'merged.csv'
+
+# CSV 파일을 데이터프레임으로 읽어옴
+df = pd.read_csv(file_path)
+
+# 각 열의 데이터 값의 갯수 출력
+for col in df.columns:
+    value_counts = df[col].value_counts()
+    print(f"Column: {col}")
+    print(value_counts)
+    print("\n")
