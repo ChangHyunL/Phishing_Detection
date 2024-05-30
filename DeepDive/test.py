13495f1f56c29e887efd509003dcdb92d5343694
eeche <dlckdgus200011@gmail.com>
2024-05-30 14:53:17 +0900

    Feat: html파일과 js파일 다운로드 DeepDive 폴더에 저장
    이를 이용해서 심층분석 진행할 예정

--- DeepDive/test.py
+++ DeepDive/test.py
@@ -1,5 +1,10 @@
+import os
 import requests
 from bs4 import BeautifulSoup
+
+# url = 'https://www.naver.com/'
+url = 'https://ssxsxs.weeblysite.com/'
+file_path = 'C:\\Users\\dlckd\\Desktop\\2024-1학기\\캡스톤디자인\\Phishing_Detection\DeepDive'
 
 
 def download_html(url):
@@ -20,12 +25,28 @@ def download_js(url):
         for js_file in js_files:
             js_response = requests.get(js_file)
             if js_response.status_code == 200:
-                js_contents.append(js_response.text)
+                js_contents.append((js_file, js_response.text))
         return js_contents
     else:
         return None
 
 
-url = 'http://example.com'
+def save_file(content, filename):
+    with open(filename, 'w', encoding='utf-8') as file:
+        file.write(content)
+
+
 html_content = download_html(url)
+if html_content:
+    save_file(html_content, os.path.join(file_path, 'index.html'))
+    # save_file(html_content, 'index.html')
+
 js_contents = download_js(url)
+if js_contents:
+    # os.makedirs('js_files', exist_ok=True)
+    counter = 1  # 파일 이름에 붙일 숫자를 세기 위한 카운터
+    for js_file, js_content in js_contents:
+        filename = f'script{counter}.js'
+        save_file(js_content, os.path.join(file_path, filename))
+        # save_file(js_content, os.path.join('js_files', filename))
+        counter += 1  # 다음 파일을 위해 카운터 증가
