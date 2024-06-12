472df3f277a01c51ff9d223cc196fded53e5fd7f
YangByeongUn <ypd06426@naver.com>
2024-06-12 22:12:19 +0900

    feat: longURL삭제, ( ishttps istrustedcert )오류 수정

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import com.example.demo.Entity.Phishing;
 import com.example.demo.Service.PhishingService;
+import com.example.demo.socketProgramming.SocketCommunication;
 import com.example.demo.socketProgramming.deepDiveSocketCommunication;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -81,6 +82,7 @@ public class MainController {
         model.addAttribute("result",result);
         model.addAttribute("wrongUrl", 0);
         System.out.println("handleFileUpload_QRCode_result = " + result);
+        model.addAttribute("ML_result", SocketCommunication.ML_result());
         return "QRcodeScanner";
     }
     @PostMapping("/scanURL")
@@ -117,6 +119,8 @@ public class MainController {
         model.addAttribute("url", url);
         model.addAttribute("result",result);
         model.addAttribute("wrongUrl", 0);
+        model.addAttribute("ML_result", SocketCommunication.ML_result());
+        System.out.println("SocketCommunication.ML_result() = " + SocketCommunication.ML_result());
 
         return "QRcodeScanner";
     }
@@ -127,11 +131,30 @@ public class MainController {
             return "redirect:scan";
         }
         System.out.println("통과");
-        Pattern pattern = Pattern.compile("(?<!id)=([0-9]+)");
+        System.out.println("deepdive_result = " + result);
+        /*
+        String regex = "isRedirection=([^,]+),\\s*(.*)";
+        Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(result);
+        String restOfString = "";
+        if (matcher.find()) {
+            String isRedirectionValue = matcher.group(1);
+            restOfString = matcher.group(2);
+            System.out.println("isRedirection=" + isRedirectionValue + ", " + restOfString);
+        } else {
+            System.out.println("No match found.");
+        }
+
         List<Integer> numbers = new ArrayList<>();
-        while (matcher.find()) {
-            numbers.add(Integer.parseInt(matcher.group(1)));
+        for (int i = 0; i < restOfString.length(); i++) {
+            char c = restOfString.charAt(i);
+            if (c == '0' || c == '1') {
+                numbers.add(Character.getNumericValue(c));
+            }
+        }*/
+        List<Integer> numbers = new ArrayList<>();
+        for(int i=0; i<15; i++){
+            numbers.add(0);
         }
         Phishing deepdive_phisihing = new Phishing(numbers);
         System.out.println("deepdive_phisihing = " + deepdive_phisihing);
